78051fee96819dc078d6bc2929ecebd2261661be
ParkJunYong <dyden119@gmail.com>
2025-11-16 20:59:42 +0900

    형용사만 학습하는 방향으로 전환 (모든 어휘를 DB에 넣기에는 오버헤드가 너무 큼)

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       <h1 className="mt-20 text-[100px] text-center font-yuji">
         Shuren - 修錬
       </h1>
+      <h2 className="text-[80px] text-center font-yuji">形容詞</h2>
+      <h3 className="text-[40px] text-center font-gowun">일본어 형용사 학습</h3>
       <LoginForm />
     </div>
   );
