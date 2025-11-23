ddffe8a06eff757073dfac2ac7bf15b913eb0aa2
ParkJunYong <dyden119@gmail.com>
2025-11-23 12:38:05 +0900

    타입파일 동기화

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,6 @@
+// ================================================
+// Enum
+// ================================================
 export enum Role {
     USER = 1,
     ADMIN = 10,
@@ -9,4 +12,31 @@ export enum Level {
     N3 = "N3",
     N4 = "N4",
     N5 = "N5",
+}
+
+// ================================================
+// 테이블 인터페이스
+// ================================================
+export interface User {
+    id: number;
+
+    email: string;
+    name: string;
+    password: string;
+    role: Role;
+    createdAt: Date;
+    updatedAt: Date;
+
+    userProgress: UserProgress;
+}
+
+export interface UserProgress {
+    id: number;
+
+    currentGrade: number;
+    exp: number;
+    isMaster: boolean;
+
+    userId: number;
+    user: User;
 }
