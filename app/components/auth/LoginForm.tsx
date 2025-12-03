df3c18ec5650472bb64189b56b7704012eab3f98
ParkJunYong <dyden119@gmail.com>
2025-12-03 10:36:21 +0900

    로그인폼에 react-hook-form 적용

--- app/components/auth/LoginForm.tsx
+++ app/components/auth/LoginForm.tsx
@@ -2,25 +2,51 @@
 import Link from "next/link";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
+import { useForm } from "react-hook-form";
+
+type LoginFormData = {
+  email: string;
+  password: string;
+};
 
 const LoginForm = () => {
-  const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    console.log("로그인");
-  };
+  const {
+    register, // input에 연결
+    handleSubmit, // 폼 제출 핸들링
+    formState: { errors, isSubmitting }, // 폼 제출 상태 관리
+  } = useForm<LoginFormData>();
+
+  const handleLogin = async (data: LoginFormData) => {};
 
   return (
-    <form className="w-[90%] md:w-[50%] mt-5 md:mt-20 mx-auto flex flex-col items-center justify-center gap-2 md:gap-4">
+    <form
+      className="w-[90%] md:w-[50%] mt-5 md:mt-20 mx-auto flex flex-col items-center justify-center gap-2 md:gap-4"
+      onSubmit={handleSubmit(handleLogin)}
+    >
       <label htmlFor="email">이메일</label>
-      <Input type="email" id="email" placeholder="이메일을 입력해주세요." />
+      <Input
+        type="email"
+        id="email"
+        placeholder="이메일을 입력해주세요."
+        {...register("email", { required: "이메일을 입력해주세요." })}
+      />
+      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
       <label htmlFor="password">비밀번호</label>
       <Input
         type="password"
         id="password"
         placeholder="비밀번호를 입력해주세요."
+        {...register("password", { required: "비밀번호를 입력해주세요." })}
       />
-      <Button type="submit" onClick={(e) => handleLogin(e)}>
-        로그인
+      {errors.password && (
+        <p className="text-red-500">{errors.password.message}</p>
+      )}
+      <Button
+        type="submit"
+        disabled={isSubmitting}
+        className={`${isSubmitting ? "bg-gray-300" : ""}`}
+      >
+        {isSubmitting ? "로그인 중..." : "로그인"}
       </Button>
       <Link href="/signup" className="w-full">
         <Button type="button">회원가입</Button>
