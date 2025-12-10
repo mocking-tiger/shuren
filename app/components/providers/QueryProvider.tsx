f5374a40f563f6b2ab5083d256d41c601fd6ce0a
ParkJunYong <dyden119@gmail.com>
2025-12-10 10:10:18 +0900

    QueryProvider 작성

---
+++ app/components/providers/QueryProvider.tsx
@@ -0,0 +1,26 @@
+"use client"; // QueryClient는 브라우저에서 상태를 관리하므로 필수
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  // QueryClient를 useState내부에서 만드는 이유: 컴포넌트 리렌더링 시 초기화 방지
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // staleTime 동안에는 fresh 상태를 유지하며 refetch를 하지 않음
+            gcTime: 5 * 60 * 1000, // gcTime 동안에는 캐시 데이터를 유지하며 가비지 컬렉션을 하지 않음
+            refetchOnWindowFocus: false, // 다른 탭에서 돌아올 때 자동으로 refetch 할지 여부
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./globals.css";
 import SizeChecker from "./components/ui/SizeChecker";
 import { Toaster } from "react-hot-toast";
 import AuthSessionProvider from "./components/providers/SessionProvider";
+import QueryProvider from "./components/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,16 +58,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${gowunBatang.variable} ${shipporiMincho.variable} ${yujiSyuku.variable} antialiased`}
       >
-        <AuthSessionProvider>
-          <div className="min-h-screen bg-[url('/images/bg/shoji2.jpg')] bg-cover bg-center flex items-center justify-center">
-            {/* 컨텐츠 블럭 */}
-            <main className="w-[90%] md:w-[70%] h-screen bg-[url('/images/bg/hanji2.jpg')]">
-              {children}
-              <Toaster />
-            </main>
-            <SizeChecker />
-          </div>
-        </AuthSessionProvider>
+        <QueryProvider>
+          <AuthSessionProvider>
+            <div className="min-h-screen bg-[url('/images/bg/shoji2.jpg')] bg-cover bg-center flex items-center justify-center">
+              <main className="w-[90%] md:w-[70%] h-screen bg-[url('/images/bg/hanji2.jpg')]">
+                {children}
+                <Toaster />
+              </main>
+              <SizeChecker />
+            </div>
+          </AuthSessionProvider>
+        </QueryProvider>
       </body>
     </html>
   );
