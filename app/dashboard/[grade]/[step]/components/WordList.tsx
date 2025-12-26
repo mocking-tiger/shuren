e8cbc2f5952fdcf71c697bd0daefb7b529edc536
ParkJunYong <dyden119@gmail.com>
2025-12-26 10:39:45 +0900

    단어 뜻, 예문 보기

--- app/dashboard/[grade]/[step]/components/WordList.tsx
+++ app/dashboard/[grade]/[step]/components/WordList.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 const WordList = ({ words }: { words: Word[] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isToggled, setIsToggled] = useState(false);
 
   const handleIndexChange = (direction: "prev" | "next") => {
     if (currentIndex === 0 && direction === "prev") {
@@ -16,31 +17,44 @@ const WordList = ({ words }: { words: Word[] }) => {
       return;
     }
     const newIndex = direction === "prev" ? currentIndex - 1 : currentIndex + 1;
+    setIsToggled(false);
     setCurrentIndex(newIndex);
   };
 
   if (!words.length) return <div>단어 정보를 불러오지 못했습니다.</div>;
   return (
-    <div className="w-full h-full flex justify-center items-center relative border-2 border-red-500">
+    <div className="w-full h-full flex justify-center items-center relative">
+      {/* 인덱스 표시 */}
       <div className="absolute top-6 text-xl font-bold">
         {currentIndex + 1} / {words.length}
       </div>
+
+      {/* 이전, 다음 */}
       <div
-        className="p-2 absolute top-[45%] left-6 text-2xl font-bold cursor-pointer"
+        className="p-2 absolute top-[45%] left-1 md:left-6 text-2xl font-bold cursor-pointer"
         onClick={() => handleIndexChange("prev")}
       >
         {"<"}
       </div>
       <div
-        className="p-2 absolute top-[45%] right-6 text-2xl font-bold cursor-pointer"
+        className="p-2 absolute top-[45%] right-1 md:right-6 text-2xl font-bold cursor-pointer"
         onClick={() => handleIndexChange("next")}
       >
         {">"}
       </div>
-      <div className="w-[80%] h-[80%] flex justify-center items-center bg-white rounded-md shadow-lg relative">
+
+      {/* 단어 박스 */}
+      <div
+        className="w-[80%] h-[80%] flex justify-center items-center bg-white rounded-md shadow-lg relative cursor-pointer"
+        onClick={() => setIsToggled(!isToggled)}
+      >
+        {/* 음성 재생 버튼 */}
         <div
-          className="absolute top-6 right-6 cursor-pointer"
-          onClick={() => runTTS(words[currentIndex].wordKana)}
+          className="w-8 h-8 md:w-10 md:h-10 absolute top-6 right-6 cursor-pointer hover:scale-110 transition-all duration-300"
+          onClick={(e) => {
+            e.stopPropagation();
+            runTTS(words[currentIndex].wordKana);
+          }}
         >
           <img
             src="/images/icon/speaker.svg"
@@ -48,9 +62,46 @@ const WordList = ({ words }: { words: Word[] }) => {
             className="w-10 h-10"
           />
         </div>
-        <h1 className="text-4xl md:text-[120px] font-bold">
+
+        <h1
+          className={`font-bold ${
+            isToggled
+              ? "text-4xl md:text-[60px] relative bottom-[100px] md:bottom-[160px]"
+              : "text-4xl md:text-[120px]"
+          }`}
+        >
           {words[currentIndex].word}
         </h1>
+        {isToggled && (
+          <div className="absolute font-sans text-center w-full">
+            <div className="text-2xl md:text-4xl">
+              {words[currentIndex].wordKana}
+            </div>
+            <div className="text-xl md:text-4xl">
+              {words[currentIndex].wordMeaning}
+            </div>
+
+            <div className="mt-10">{words[currentIndex].exampleKana}</div>
+            <div className="relative w-full">
+              <div className="text-xl md:text-4xl">
+                {words[currentIndex].example}
+              </div>
+              <img
+                src="/images/icon/speaker.svg"
+                alt="speaker"
+                className="w-10 h-10 absolute top-0 right-6"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  runTTS(words[currentIndex].exampleKana);
+                }}
+              />
+            </div>
+
+            <div className="text-xl md:text-4xl">
+              {words[currentIndex].exampleMeaning}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
